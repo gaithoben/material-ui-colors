aaa37700a274c4b2667c7eee22ab12f297113757
Bernard Gaitho <gaithoben@gmail.com>
2019-03-23 22:15:14 +0300

    feat(release): First release
    First release

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,12 @@
 import { expect } from 'chai';
 
-import {red} from './index';
+import { red } from './index';
 
 describe('Test Colors', function() {
   describe('color', function() {
-    it('Should return a string', function() {
+    it('Should return a color code in hex', function() {
       expect(red[500]).to.include('#');
+      console.log(red[500]);
     });
   });
 });
